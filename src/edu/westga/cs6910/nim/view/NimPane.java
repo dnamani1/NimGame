a167da41bf15316ae1f64abc99ca012c70b12b01
Deeksha Namani <dnamani1@my.westga.edu>
2023-06-18 08:40:37 -0500

    Added menu to the application.

--- src/edu/westga/cs6910/nim/view/NimPane.java
+++ src/edu/westga/cs6910/nim/view/NimPane.java
@@ -4,8 +4,12 @@ import edu.westga.cs6910.nim.model.Game;
 import edu.westga.cs6910.nim.model.Player;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -27,17 +31,13 @@ public class NimPane extends BorderPane {
 
 	/**
 	 * Creates a pane object to provide the view for the specified Game model
-	 * object.
-	 * 1. Using the 'first player chooser pane' as a guide
-     * Create an HBox with the appropriate style, then
-	 * make a human player pane and add it to the HBox.
-     * Finally add the HBox to the content pane
-     * 
-     * 2. Using the other panes as a guide, create
-     * and add a status pane
-     * 
-     * 3. Using the other panes as a guide, create 
-     * and add a computer pane
+	 * object. 1. Using the 'first player chooser pane' as a guide Create an HBox
+	 * with the appropriate style, then make a human player pane and add it to the
+	 * HBox. Finally add the HBox to the content pane
+	 * 
+	 * 2. Using the other panes as a guide, create and add a status pane
+	 * 
+	 * 3. Using the other panes as a guide, create and add a computer pane
 	 * 
 	 * @param theGame the domain model object representing the Nim game
 	 * 
@@ -48,6 +48,8 @@ public class NimPane extends BorderPane {
 		this.theGame = theGame;
 
 		this.pnContent = new BorderPane();
+
+		this.setTop(this.buildMenuBar());
 
 		this.addFirstPlayerChooserPane(theGame);
 
@@ -61,23 +63,78 @@ public class NimPane extends BorderPane {
 		hboxStatus.getStyleClass().add("pane-border");
 		this.pnGameInfo = new StatusPane(theGame);
 		hboxStatus.getChildren().add(this.pnGameInfo);
-	    this.pnContent.setCenter(hboxStatus);
+		this.pnContent.setCenter(hboxStatus);
 
-	    HBox hboxComputerPlayer = new HBox();
-	    hboxComputerPlayer.getStyleClass().add("pane-border");
-	    this.pnComputerPlayer = new ComputerPane(theGame);
-	    hboxComputerPlayer.getChildren().add(this.pnComputerPlayer);
-        this.pnContent.setRight(hboxComputerPlayer);
+		HBox hboxComputerPlayer = new HBox();
+		hboxComputerPlayer.getStyleClass().add("pane-border");
+		this.pnComputerPlayer = new ComputerPane(theGame);
+		hboxComputerPlayer.getChildren().add(this.pnComputerPlayer);
+		this.pnContent.setRight(hboxComputerPlayer);
 
 		this.setCenter(this.pnContent);
 	}
 
+	/**
+	 * This method creates the menu bar with menu and menu items.
+	 * 
+	 * @return menu bar.
+	 */
+	private MenuBar buildMenuBar() {
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(this.createGameMenu(), this.createStrategyMenu());
+		return menuBar;
+	}
+
+	/**
+	 * This method creates menu game, with exit menu items.
+	 * 
+	 * @return game menu.
+	 */
+	private Menu createGameMenu() {
+		Menu gameMenu = new Menu("Game");
+		MenuItem exitItem = new MenuItem("Exit");
+		exitItem.setMnemonicParsing(true);
+		exitItem.setAccelerator(KeyCombination.keyCombination("Ctrl+X"));
+		exitItem.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.exit(0);
+			}
+		});
+		gameMenu.getItems().add(exitItem);
+		return gameMenu;
+	}
+
+	/**
+	 * This method creates strategy menu with caution, greedy, random menu items.
+	 * 
+	 * @return strategy menu.
+	 */
+	private Menu createStrategyMenu() {
+		Menu strategyMenu = new Menu("Strategy");
+
+		MenuItem cautiousItem = new MenuItem("Cautious");
+		cautiousItem.setMnemonicParsing(true);
+		cautiousItem.setAccelerator(KeyCombination.keyCombination("Ctrl+C"));
+
+		MenuItem greedyItem = new MenuItem("Greedy");
+		greedyItem.setMnemonicParsing(true);
+		greedyItem.setAccelerator(KeyCombination.keyCombination("Ctrl+E"));
+
+		MenuItem randomItem = new MenuItem("Random");
+		randomItem.setMnemonicParsing(true);
+		randomItem.setAccelerator(KeyCombination.keyCombination("Ctrl+R"));
+
+		strategyMenu.getItems().addAll(cautiousItem, greedyItem, randomItem);
+		return strategyMenu;
+	}
+
 	/*
-     * Adding Choose First Player pane on top  of the content pane.
-     * 
-     * @param theGame the Game object
-     * 
-     */
+	 * Adding Choose First Player pane on top of the content pane.
+	 * 
+	 * @param theGame the Game object
+	 * 
+	 */
 	private void addFirstPlayerChooserPane(Game theGame) {
 		HBox topBox = new HBox();
 		topBox.getStyleClass().add("pane-border");
@@ -107,8 +164,7 @@ public class NimPane extends BorderPane {
 		}
 
 		/**
-		 * Instantiate the computer player button and set
-		 * its action listener.
+		 * Instantiate the computer player button and set its action listener.
 		 * 
 		 * Create a ToggleGroup and add the 2 radio buttons to it.
 		 * 
@@ -122,15 +178,15 @@ public class NimPane extends BorderPane {
 			this.radHumanPlayer.setOnAction(new HumanFirstListener());
 
 			this.radComputerPlayer = new RadioButton(this.theComputer.getName() + " first");
-            this.radComputerPlayer.setOnAction(new ComputerFirstListener());
+			this.radComputerPlayer.setOnAction(new ComputerFirstListener());
 
-            ToggleGroup toggleGroup = new ToggleGroup();
-            this.radHumanPlayer.setToggleGroup(toggleGroup);
-            this.radComputerPlayer.setToggleGroup(toggleGroup);
+			ToggleGroup toggleGroup = new ToggleGroup();
+			this.radHumanPlayer.setToggleGroup(toggleGroup);
+			this.radComputerPlayer.setToggleGroup(toggleGroup);
 
-            this.add(this.radHumanPlayer, 0, 0);
-            this.add(this.radComputerPlayer, 1, 0);
-           
+			this.add(this.radHumanPlayer, 0, 0);
+			this.add(this.radComputerPlayer, 1, 0);
+
 		}
 
 		/*
@@ -157,14 +213,13 @@ public class NimPane extends BorderPane {
 			 * Sets up user interface and starts a new game. Event handler for a click in
 			 * the human player button.
 			 * 
-			 * Enable the human player pane and start a game
-			 *  with the human playing first.
+			 * Enable the human player pane and start a game with the human playing first.
 			 */
 			@Override
 			public void handle(ActionEvent event) {
 				NimPane.this.pnChooseFirstPlayer.setDisable(true);
 				NimPane.this.pnHumanPlayer.setDisable(false);
-                NimPane.this.theGame.startNewGame(NewGamePane.this.theHuman);
+				NimPane.this.theGame.startNewGame(NewGamePane.this.theHuman);
 			}
 		}
 	}
